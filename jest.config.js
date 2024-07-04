b78d45e775eaa8d4821eafe5e9fed0000d58f81b
JrGomes <jr.gomes.sep@gmail.com>
2024-07-04 00:21:58 -0300

    chore: add prisma

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,21 @@
 const config = {
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**.ts'],
-    coverageDirectory: 'coverage',
-    coverageProvider: 'babel',
-    moduleNameMapper: {
-      '@/tests/(.+)': '<rootDir>/tests/$1',
-      '@/(.+)': '<rootDir>/src/$1'
-    },
-    roots: [
-      '<rootDir>/src',
-      '<rootDir>/tests'
-    ],
-    transform: {
-      '\\.ts$': 'ts-jest'
-    }
-  }
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/mocks/(.+)': '<rootDir>/tests/mocks/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/config/prisma-client.ts']
+}
 
-  module.exports = config
+module.exports = config
