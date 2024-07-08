cf683cc95608bcdc7f6fbd5325cab23651402de4
JrGomes <jr.gomes.sep@gmail.com>
2024-07-08 15:28:20 -0300

    test(location-controller): add missing tests to complete coverage

--- tests/infrastructure/express/controllers/location-controller.spec.ts
+++ tests/infrastructure/express/controllers/location-controller.spec.ts
@@ -30,6 +30,28 @@ describe('LocationController', () => {
     expect(res.json).toHaveBeenCalledWith({ id: 1, name: location.name })
   })
 
+  it('should return 400 if location does not have name', async () => {
+    const location = { name: '' }
+    req.body = location
+
+    locationService.createLocation.mockResolvedValue(new Error('Location must be have name!'))
+    await locationController.createLocation(req as Request, res as Response)
+
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith({ error: 'Location must be have name!' })
+  })
+
+  it('should return 500 when trying to Create location if there is an error', async () => {
+    const location = { name: 'Location' }
+    req.body = location
+
+    locationService.createLocation.mockRejectedValue(null)
+    await locationController.createLocation(req as Request, res as Response)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' })
+  })
+
   it('should update a location name', async () => {
     const location = { id: 1, name: 'Updated Location' }
     req.body = location
@@ -41,6 +63,17 @@ describe('LocationController', () => {
     expect(res.json).toHaveBeenCalledWith({ id: location.id, name: location.name })
   })
 
+  it('should return 500 when trying to update location name if there is an error', async () => {
+    const location = { name: 'Update Location' }
+    req.body = location
+
+    locationService.updateLocationName.mockRejectedValue(null)
+    await locationController.updateLocationName(req as Request, res as Response)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' })
+  })
+
   it('should get all locations', async () => {
     const locations = [{ id: 1, name: 'Location 1' }, { id: 2, name: 'Location 2' }]
     locationService.getAllLocations.mockResolvedValue(locations)
@@ -49,6 +82,14 @@ describe('LocationController', () => {
 
     expect(res.status).toHaveBeenCalledWith(200)
     expect(res.json).toHaveBeenCalledWith(locations)
+  })
+
+  it('should return 500 when trying to get all locations if there is an error', async () => {
+    locationService.getAllLocations.mockRejectedValue(null)
+    await locationController.getAllLocations(req as Request, res as Response)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' })
   })
 
   it('should get a location by id', async () => {
@@ -70,5 +111,16 @@ describe('LocationController', () => {
 
     expect(res.status).toHaveBeenCalledWith(404)
     expect(res.json).toHaveBeenCalledWith({ error: 'Location not found' })
+  })
+
+  it('should return 500 when trying to get location by id if there is an error', async () => {
+    const location = { id: 1 }
+    req.body = location
+
+    locationService.getLocationById.mockRejectedValue(null)
+    await locationController.getLocationById(req as Request, res as Response)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' })
   })
 })
