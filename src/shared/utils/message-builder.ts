222f194805613382fca1defecf83a6644addd96f
JrGomes <jr.gomes.sep@gmail.com>
2024-07-10 16:03:36 -0300

    feat: add shared utils MessageBuilder and HttpStatus

---
+++ src/shared/utils/message-builder.ts
@@ -0,0 +1,15 @@
+export class MessageBuilder {
+  constructor (private readonly context: string) {}
+
+  missingParam (param: string, prefix?: string): string {
+    return `${prefix ?? this.context} must have ${param}`
+  }
+
+  notFound (prefix?: string): string {
+    return `${prefix ?? this.context} not found`
+  }
+
+  internalServerError (): string {
+    return 'Internal Server Error'
+  }
+}
---
+++ tests/shared/utils/message-builder.spec.ts
@@ -0,0 +1,33 @@
+import { MessageBuilder } from '@/shared/utils'
+
+describe('MessageBuilder', () => {
+  test('missingParam sem prefixo', () => {
+    const builder = new MessageBuilder('Context')
+    const result = builder.missingParam('parametro')
+    expect(result).toBe('Context must have parametro')
+  })
+
+  test('missingParam com prefixo', () => {
+    const builder = new MessageBuilder('Context')
+    const result = builder.missingParam('parametro', 'Prefixo')
+    expect(result).toBe('Prefixo must have parametro')
+  })
+
+  test('notFound sem prefixo', () => {
+    const builder = new MessageBuilder('Context')
+    const result = builder.notFound()
+    expect(result).toBe('Context not found')
+  })
+
+  test('notFound com prefixo', () => {
+    const builder = new MessageBuilder('Context')
+    const result = builder.notFound('Prefixo')
+    expect(result).toBe('Prefixo not found')
+  })
+
+  test('internalServerError', () => {
+    const builder = new MessageBuilder('Context')
+    const result = builder.internalServerError()
+    expect(result).toBe('Internal Server Error')
+  })
+})
